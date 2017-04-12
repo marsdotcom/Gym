ba70df2dfc4fde33666387ba0ab8e3a14e52d8ec
marsdotcom <mars.exe@live.com>
2017-04-12 10:47:26 +0300

    Initial commit

---
+++ app/src/main/java/com/example/koldun/gym/MainActivity.java
@@ -0,0 +1,92 @@
+package com.example.koldun.gym;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Chronometer;
+import android.os.SystemClock;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Chronometer mChronometer;
+    boolean running;
+    Button mButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        running = false;
+
+        mChronometer = (Chronometer) findViewById(R.id.chronometer2);
+        mChronometer.setFormat("%s");
+
+        mButton = (Button)findViewById(R.id.button);
+        mButton.setOnClickListener(this);
+
+
+        mChronometer.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
+            @Override
+            public void onChronometerTick(Chronometer chronometer) {
+
+            }
+        });
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Snackbar.make(view, "7777", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if(running = !running){
+            mChronometer.setBase(SystemClock.elapsedRealtime());
+            mChronometer.start();
+            mButton.setText("Stop");
+
+        }else{
+            mButton.setText("Start");
+            mChronometer.stop();
+
+        }
+
+    }
+}
